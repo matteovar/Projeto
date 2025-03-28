db31ffddba3736261682e33d624e033b104a84c3
matteovar <matvarnier@gmail.com>
2025-03-28 15:48:48 -0300

    Added cards

--- main.py
+++ main.py
@@ -1,58 +1,95 @@
-########################
-# Import Libraries
-
 import streamlit as st
 import pandas as pd
 import altair as alt
 import plotly.express as px
+import plotly.graph_objects as go
 
-########################
-# Page config
+
 st.set_page_config(
-    page_title="US foster kids",
+    page_title="US Foster Care Dashboard",
     layout="wide"
 )
-alt.themes.enable('dark')
-
-########################
-# Title
 
 st.markdown("<h1 style='text-align: center;'>Children in Foster Care in the United States</h1>", 
             unsafe_allow_html=True)
-######################## 
-# Load file
-df = pd.read_csv('data/adoptios_Usa_2013_2022.csv')
-df_adopted = pd.read_csv('data/estates_adopteds.csv')
-df_served = pd.read_csv('data/estates_served.csv')
 
-########################
-    # Define format Year and put Year as fixed X
+df = pd.read_csv('data/adoptios_Usa_2013_2022.csv')
+
 df["Year"] = pd.to_datetime(df["Year"], format="%Y")
 df.set_index("Year", inplace=True)
 
-######################## 
-# Columns
-col = st.columns((2.5,4.5,2.5), gap='medium')
-
-def nation_kids():
-    st.markdown('''
-    ### National Trends: Children in Foster Care vs. Adopted (2013-2022)
-    ''')
-    st.line_chart(df[['Served', "Adopted"]])
-    
-    
+def card1():
+    # Resetando o índice
     df.reset_index(inplace=True)
 
-    # Selecionar os dados de 2013 e 2022
+    # Selecionar os dados de 2013 e 2021
     adopted_2013 = df.loc[df["Year"].dt.year == 2013, "Adopted"].values[0]
     adopted_2021 = df.loc[df["Year"].dt.year == 2021, "Adopted"].values[0]
 
     # Calcular o crescimento percentual
     growth_percentage = ((adopted_2021 - adopted_2013) / adopted_2013) * 100
     
-    st.markdown(f'''
-    #### From 2013 to 2022, the adoption rate from foster care increased by :green[↑{growth_percentage:.2f}%] nationally
+    return adopted_2021, growth_percentage  
+
+def card2():
+    # Resetando o índice
+    df.reset_index(inplace=True)
+
+    # Selecionar os dados de 2013 e 2021
+    served_2013 = df.loc[df["Year"].dt.year == 2013, 'Served'].values[0]
+    served_2021 = df.loc[df["Year"].dt.year == 2021, 'Served'].values[0]
+
+    # Calcular o crescimento percentual (2013 para 2021)
+    growth_served = ((served_2021 - served_2013) / served_2013) * 100
+    
+    return served_2021, growth_served  
+
+def cards():
+    # Chama as funções card1 e card2 passando o df
+    adopted_2021, growth_percentage = card1()
+    served_2021, growth_served = card2()
+    
+    # Definindo os valores para o cartão
+    data = {
+        'Label': ['Adopted', 'Served', 'Growth of Adoptions', 'Total Children'],
+        'Value': [f'{adopted_2021:1}', served_2021, 56.7, 912345],
+        'Delta': [f'{growth_percentage:.2f}%', f'{growth_served:.2f}%', 12.3, 313]  
+    }   
+    
+    df_cards = pd.DataFrame(data)
+
+    # Dividindo o layout em 4 colunas
+    cols = st.columns(4)  
+
+    
+    for idx, row in df_cards.iterrows():
+        col_idx = idx % 4
+        with cols[col_idx]:
+            if row['Delta'] is not None:
+                # Usando st.metric para exibir o valor e a variação percentual
+                st.metric(
+                    label=row['Label'],
+                    value=row['Value'],
+                    delta=row['Delta'],
+                    border = True
+                )
+            else:
+                st.write(f"{row['Label']}: {row['Value']}")
+
+cards()
+
+df_adopted = pd.read_csv('data/estates_adopteds.csv')
+df_served = pd.read_csv('data/estates_served.csv')
+
+col = st.columns((2.5, 4.5, 2.5), gap='medium')
+col1, col2, col3 = st.columns(3)
+
+def nation_kids():
+    st.markdown('''
+    
     ''')
+    st.line_chart(df[['Served', "Adopted"]])
+
 
 def states_kids():
     st.subheader("States Data ")
@@ -85,12 +122,12 @@ def states_kids():
     
 def age():
     st.markdown('''
-    ### Key Foster Care Statistics (2021)
+    
     ''')
     
     df = pd.DataFrame(
         {
-            "Year": ["2021"] * 5,  # Repetindo "2021" para cada linha
+            "Year": ["2021"] * 5,  
             "Age": ['< 1', '1 to 5', '6 to 10', '11 to 15', '16 to 20'],
             "Number": [28.690, 133.049, 87.383, 86.793, 55.396],
         }
@@ -132,6 +169,7 @@ def gender():
     
     st.plotly_chart(fig1, use_container_width=True)
     
+
 with col[0]:
     
     age()
